5592238153ab8c0c2d8791d4f21e59eebbfb8ae9
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-04-12 10:39:19 +0530

    3rd commit

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require('path')
 var bookRouter = require('./book')
 var userRouter = require('./userRoute')
 
@@ -13,20 +13,29 @@ router.use(express.json())
 const uri = "mongodb://localhost:27017/bookStore"
 //connect to MongoDB
 try {
-  mongoose.connect(uri,{
+  mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   console.log("Connected to MongoDB")
 } catch (error) {
-  console.error("Error  => "+error)
+  console.error("Error  => " + error)
 }
-router.use("/book",bookRouter);
+router.use("/book", bookRouter);
 router.use("/user", userRouter);
 
+
+// deployment 
+
+  const dirpath = path.resolve();
+  router.use(express.static("front/dist"));
+  router.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirpath, 'front/dist/index.html'));
+  });
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 
 module.exports = router;
